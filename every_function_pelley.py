b55d687a32593d8e7f76e3fba975fe6236de39eb
Blake <pelleyb28@cbchs.org>
2024-09-19 12:44:06 -0500

    Every_fun_pelley

---
+++ every_function_pelley.py
@@ -0,0 +1,6 @@
+Resturants = ['Mcdonalds', 'Tacobell','KFC','Dominoes']
+print = (Resturants)
+print("Here is the original list:")
+print(Resturants)
+print("\nHere is the sorted list:")
+print(sorted(Resturants))
